40c4fec971605c8330224c88a21b9ed1ed504aea
Stuart Weenig <stuart@weenig.com>
2020-03-24 12:46:58 -0500

    Simplified example and removed unneeded functions.

--- DataSources/Groovy/SNMP/Examples/if_listallnames.groovy
+++ DataSources/Groovy/SNMP/Examples/if_listallnames.groovy
@@ -20,58 +20,9 @@ def snmpMapToTable(snmpmap, int wildvalueTerms = 1) {
     return rows
 }
 
-def sortSnmpWalkTable(table, int wildvalueTerms = 1) {
-    if (wildvalueTerms > 2) {
-        println("More than 2 terms in the wildvalue are not supported.");
-        return [];
-    }
-    if (wildvalueTerms == 1) {
-        rowsWildvalueSorted = table.keySet().collect { it.toInteger() }.sort()
-    } else {
-        rowsWildvalueSorted = table.keySet().collect { Float.parseFloat(it)}.sort()
-    }
-    return rowsWildvalueSorted
-}
-
-def sortDataKeys(data){
-    if (data.keySet().size() > 1){ //if there is more than one data point per wildvalue
-        if (data.keySet()[0].tokenize(".").size() > 1){return data.keySet()} //if there is more than one term in the data key
-        else {
-            println("Data sorted by column ID")
-            return data.keySet().collect { it.toInteger() }.sort().collect{it.toString()}
-        }
-    } else {return data.keySet()}
-}
-
-def pprintSnmpWalkTable(table) {
-    if (table.size() == 0) {
-        println("No data returned.")
-    } else {
-        sortSnmpWalkTable(table).each { k ->
-            println("Wildvalue: ${k.toString()}:")
-            data = table[k.toString()]
-            sortDataKeys(data).each {println("  ${it ?: Oid.tokenize(".")[-1]}.${"##"+"WILDVALUE"+"##"}: ${data[it]}")}
-        }
-    }
-}
-
-def flattenSnmpWalkTable(table) {
-    flat = []
-    sortSnmpWalkTable(table).each { k ->
-        data = table[k.toString()].each {key, val ->
-            flat << [k,key, val]
-        }
-    }
-    return flat
-}
-
 ifEntryRaw = snmpMapToTable(Snmp.walkAsMap(host, "1.3.6.1.2.1.2.2.1", props, timeout))
 ifXEntryRaw = snmpMapToTable(Snmp.walkAsMap(host, "1.3.6.1.2.1.31.1.1.1", props, timeout))
 ifEntryRaw.each {wildvalue, data ->
-    println("""Interface ${wildvalue}:
-	ifAlias: ${ifXEntryRaw[wildvalue]["1"]}
-	ifDescr: ${data["2"]}
-	ifType: ${data["3"]}
-""")
+    println("Interface ${wildvalue}:\tifAlias: ${ifXEntryRaw[wildvalue]["1"]}\tifDescr: ${data["2"]}\tifType: ${data["3"]}")
 }
 return 0
