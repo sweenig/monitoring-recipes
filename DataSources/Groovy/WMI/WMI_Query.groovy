8501c5dbd89e95433bb875a14ff59ce3b642d74a
Stuart Weenig <stuart@weenig.com>
2020-08-20 08:24:47 -0500

    General cleanup. Corrected loop through output_all.

--- DataSources/Groovy/WMI/WMI_Query.groovy
+++ DataSources/Groovy/WMI/WMI_Query.groovy
@@ -5,59 +5,54 @@
 import com.santaba.agent.groovyapi.win32.WMI
 
 // Set hostname variable.
-def hostname = hostProps.get("system.hostname");
+def hostname = hostProps.get("system.hostname")
 
 // Try the following code.
 try {
+  // This is our namespace, we're using the default value here to illustrate
+  // how to pass it if your object isn't in root\cimv2
+  // This parameter is optional.
+  def namespace = "CIMv2"
 
-    // This is our namespace, we're using the default value here to illustrate
-    // how to pass it if your object isn't in root\cimv2
-    // This parameter is optional.
-    def namespace = "CIMv2";
+  // You can also pass an optional timeout value, the default is 30 seconds
+  def timeout = 30
 
-    // You can also pass an optional timeout value, the default is 30 seconds
-    def timeout = 30;
+  // This is our WMI query.
+  def wmi_query = 'select * from win32_operatingsystem'
 
-    // This is our WMI query.
-    def wmi_query = 'select * from win32_operatingsystem';
+/*
+  - Query All: captures all values of the given query, returns a list of maps.
+  - WMI.queryAll(hostname, namespace, wmi_query, timeout)
 
-    /*
-    - Query All: captures all values of the given query, returns map.
-        - WMI.queryAll(hostname, namespace, wmi_query, timeout)
+  - Query First: captures only the first output value of the query, returns map.
+  - WMI.queryFirst(hostname, namespace, wmi_query, timeout)
+*/
 
-    - Query First: captures only the first output value of the query, returns map.
-        - WMI.queryFirst(hostname, namespace, wmi_query, timeout)
-     */
+  def wmi_output_all = WMI.queryAll(hostname, namespace, wmi_query, timeout)
+  def wmi_output_first = WMI.queryFirst(hostname, namespace, wmi_query, timeout)
 
-    def wmi_output_all = WMI.queryAll(hostname, namespace, wmi_query, timeout);
-    def wmi_output_first = WMI.queryFirst(hostname, namespace, wmi_query, timeout);
+  // Print out the output of our WMI query.
+  println '*** Query all output:' //outputs a list of maps
+  println wmi_output_all
 
-    // Print out the output of our WMI query.
-    println '*** Query all output:'
-    println wmi_output_all;
+  println '*** Query first output:'
+  println wmi_output_first
 
-    println '*** Query first output:'
-    println wmi_output_first;
-
-    // example of iterating through the map of values.
-    wmi_output_all.each {
-        k, v ->
-
-            // print k=v
-            println "${k}=${v}"
+  // example of iterating through the map of values.
+  wmi_output_all.each {entry ->
+    entry.each{k,v->
+      // print k=v
+      println "${k}=${v}"
     }
+  }
 
-    /*
-    - The rest of your post processing code goes here.
-    */
+  /*
+  - The rest of your post processing code goes here.
+  */
 
-    // Exit by returning 0.
-    return 0;
+  // Exit by returning 0.
+  return 0
 }
 
 // Catch any exceptions that may have occurred.
-catch (Exception e) {
-    // Print exception out.
-    println e
-    return 1;
-}
+catch (Exception e) {println e;return 1}
